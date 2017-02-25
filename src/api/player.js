075092f0950393ae6053188c016cceeb5ab81f72
Thomas Alberola <thomas.alberola@ekino.fr>
2017-02-25 10:33:22 +0100

    feat(mock): Moving the mock data to the REST API package

--- src/api/player.js
+++ src/api/player.js
@@ -1,23 +1,10 @@
 import axios from 'axios';
 import { api as apiConfig } from './config';
 
-
-export const getPlayers = (next) => {
-  axios.get(`${apiConfig.url}/players/`)
-    .then((response) => {
-      next(response);
-    })
-    .catch(function (error) {
-      next({error: error});
-    });
+export const getPlayers = () => {
+  return axios.get(`${apiConfig.url}/players/`);
 };
 
-export const kickPlayer = (steamId, next) => {
-  axios.get(`${apiConfig.url}/players/${steamId}/kick`)
-    .then((response) => {
-      next(response.data);
-    })
-    .catch(function (error) {
-      next({error: error});
-    });
+export const kickPlayer = (steamId) => {
+  return axios.get(`${apiConfig.url}/players/${steamId}/kick`);
 };
--- src/components/containers/Players/PlayerListContainer.js
+++ src/components/containers/Players/PlayerListContainer.js
@@ -1,17 +1,8 @@
 import React, { Component } from 'react';
-import _ from 'lodash';
 
 import { getPlayers, kickPlayer} from '../../../api/player';
 
 import PlayerList from '../../presentationals/Players/PlayersList';
-
-const playersMocks = [
-  { name: 'Shug0', client: { steamId: _.random(1, 99999999999)} },
-  { name: 'Zoski', client: { steamId: _.random(1, 99999999999)} },
-  { name: 'Scrutch', client: { steamId: _.random(1, 99999999999)} },
-  { name: 'Daft Spirit', client: { steamId: _.random(1, 99999999999)} },
-  { name: 'KevinDu33', client: { steamId: _.random(1, 99999999999)} },
-];
 
 class PlayerListContainer extends Component {
   constructor() {
@@ -21,7 +12,7 @@ class PlayerListContainer extends Component {
     this.handleGetPlayers = this.handleGetPlayers.bind(this);
 
     this.state = {
-      players: playersMocks,
+      players: [],
       playersLoaded: false,
     };
   }
@@ -30,22 +21,31 @@ class PlayerListContainer extends Component {
     this.setState({
       playersLoaded: false
     });
-    getPlayers((response) => {
-      if (response.status === 200) {
-        this.setState({
-          players: response.data.length ? response.data : playersMocks,
-          playersLoaded: true
-        });
-      }
-    });
+
+    getPlayers()
+      .then((response) => {
+        if (response.status === 200) {
+          this.setState({
+            players: response.data,
+            playersLoaded: true
+          });
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   handleKickPlayer(steamId) {
-    kickPlayer(steamId, (response) => {
-      if (response.status === 200) {
-        console.log('Player kicked');
-      }
-    });
+    kickPlayer(steamId)
+      .then((response) => {
+        if (response.status === 200) {
+          console.log('Player kicked');
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   componentDidMount() {
